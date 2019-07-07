972f2e8e12a23e99e346321408569aed2768939d
Rutger McKenna <rutgerjmckenna@gmail.com>
2019-07-07 19:31:44 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -27,10 +27,9 @@ end
 def reduce_to_all_true(source_array)
   i = 0 
   while i < source_array.length
-  if i = false
+  if source_array.include?(false)
     return false
   else
-    i != true 
     return true
   end
   i += 1 
